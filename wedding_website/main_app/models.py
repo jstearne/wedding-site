71448af4fcf5e5d2105e9b3e7f4c09fae2d723a6
Jared Stearne <jared.stearne@gmail.com>
2021-11-03 19:42:40 -0700

    ERD returned

--- wedding_website/main_app/models.py
+++ wedding_website/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+class Guest
