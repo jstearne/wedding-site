c3b33428aed53e7cdf02a210c63c86855c63f195
Jared Stearne <jared.stearne@gmail.com>
2021-11-04 15:02:09 -0700

    Added whitenoise, static_root correction.

--- wedding_website/wedding_website/settings.py
+++ wedding_website/wedding_website/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # creates nested media/media
 
