a799cbdb914dd2195d990a0c54066a51129d6bb4
Jared Stearne <jared.stearne@gmail.com>
2021-11-04 15:21:55 -0700

    Disabled static, can't deploy with it and fixes aren't working. Heroku just recommends disabling it.

--- wedding_website/wedding_website/settings.py
+++ wedding_website/wedding_website/settings.py
@@ -120,11 +120,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# URL is where my static images will reside on server once deployed
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # creates nested media/media
 
