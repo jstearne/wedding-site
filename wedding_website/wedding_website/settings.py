e3d7d01cba760087e3554dd3eab8da7beb4c24a6
Jared Stearne <jared.stearne@gmail.com>
2021-11-03 20:34:23 -0700

    Image uploading almost done. Test out via django admin.

--- wedding_website/wedding_website/settings.py
+++ wedding_website/wedding_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,7 +122,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SET ME LAST!
+# LOGIN_REDIRECT_URL = '/artists/'
+# LOGOUT_REDIRECT_URL = 'https://myflexspace.herokuapp.com/'
