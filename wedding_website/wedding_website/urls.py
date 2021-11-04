566c3812e682e031e15b46f74ecaf59dccb46d94
Jared Stearne <jared.stearne@gmail.com>
2021-11-03 19:32:51 -0700

    Update

--- wedding_website/wedding_website/urls.py
+++ wedding_website/wedding_website/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('', include('main_app.urls')), # allows django to see main_app/urls.py
 ]
